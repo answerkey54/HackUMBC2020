84f7a680657eb73d7e625e84d4dcc35655c06224
aayush54 <72360770+aayush54@users.noreply.github.com>
2020-11-15 08:31:55 -0500

    Finished App :)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <title>Dank and Aayush's Budget Buddy!</title> 
         <FormApp />
-        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://neuvoo.com/tax-calculator/"
@@ -24,7 +24,11 @@ function App() {
 class FormApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      income: 0,
+      taxes: 0,
+      debt:0
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -46,6 +50,7 @@ class FormApp extends React.Component {
 
   render() {
     return (
+      <React.Fragment>
       <form>
         <label>
           Income:
@@ -66,38 +71,181 @@ class FormApp extends React.Component {
             onChange={this.handleChange} />
         </label>
 
+        <label>
+          Debt:
+          <input
+            name="debt"
+            type="number"
+            value={this.state.debt}
+            onChange={this.handleChange} />
+        </label>
+
       </form>
+      <Table taxes={this.state.taxes} income = {this.state.income} debt = {this.state.debt}/>
+      </React.Fragment>
     );
+  }
+
+}
+
+
+class Table extends React.Component {
+  constructor(props) {
+     super(props)
+     this.props = {
+       taxes: 0,
+       income: 0,
+       debt:0
+     }
+     this.state = {
+        rent: 0,
+        utilities:0,
+        insurance:0,
+        transport:0,
+        personal:0,
+        food:0,
+        savings:0,
+        retirement:0,
+        miscellaneous:0,
+        payment:0
+     }
+  }
+
+
+  componentDidUpdate(prevProps){
+    if(prevProps.income != this.props.income ||
+       prevProps.debt != this.props.debt || prevProps.taxes != this.props.taxes){
+      this.calculate();
+    }
+  }
+
+  
+  calculate(){
+    var cash = (this.props.income-this.props.taxes)/12;
+   
+    var payment1=0;
+    if (this.props.debt>0){
+      if (this.props.debt<.05*cash){
+        payment1=this.props.debt;
+        cash=cash-payment1;
+      }
+      else{
+      payment1=.05*cash;
+      cash=cash-payment1;
+      }
+    }
+
+    //Necesities
+    var foodMin = 250;
+    var food1 = .1*cash;
+    if (food1<foodMin){
+      food1=foodMin;
+      cash=cash-food1;
+    }
+    var rent1 = .35*cash;
+    var utilities1 = .05*cash;
+    var insurance1 = .05*cash;
+    var transport1 = .15*cash;
+    var personal1 = .05*cash;
+
+    //Extra
+    var savings1=.1*cash;
+    var retirement1 = .1*cash;
+    var miscellaneous1 = .05*cash;
+
+    
+    //var output = [rent,utilities,insurance,transport,personal,food,savings,retirement,miscellaneous,payment];
+    rent1 = Math.round(rent1 * 100) / 100;
+    utilities1 = Math.round(utilities1 * 100) / 100;
+    insurance1 = Math.round(insurance1 * 100) / 100;
+    transport1 = Math.round(transport1 * 100) / 100;
+    personal1 = Math.round(personal1 * 100) / 100;
+    food1 = Math.round(food1 * 100) / 100;
+    savings1 = Math.round(savings1 * 100) / 100;
+    retirement1 = Math.round(retirement1 * 100) / 100;
+    miscellaneous1 = Math.round(miscellaneous1 * 100) / 100;
+    payment1 = Math.round(payment1 * 100) / 100;
+
+      this.setState({
+        rent: rent1,
+        utilities:utilities1,
+        insurance: insurance1,
+        transport: transport1,
+        personal: personal1,
+        food: food1,
+        savings: savings1,
+        retirement: retirement1,
+        miscellaneous: miscellaneous1,
+        payment: payment1
+      });
+      
+    
+  }
+
+  
+
+  render() {
+     return (
+      <table>
+      <thead>
+          <tr>
+              <th colspan="2">Budget</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr >
+              <td>Rent</td>
+              <td>{this.state.rent}</td>
+          </tr>
+          <tr>
+              <td>Utilities</td>
+              <td>{this.state.utilities}</td>
+          </tr>
+          <tr>
+              <td>Insurance</td>
+              <td>{this.state.insurance}</td>
+          </tr>
+          <tr>
+              <td>Transport</td>
+              <td>{this.state.transport}</td>
+          </tr>
+          <tr>
+              <td>Personal</td>
+              <td>{this.state.personal}</td>
+          </tr>
+          <tr>
+              <td>Food</td>
+              <td>{this.state.food}</td>
+          </tr>
+          <tr>
+              <td>Savings</td>
+              <td>{this.state.savings}</td>
+          </tr>
+          <tr>
+              <td>Retirement</td>
+              <td>{this.state.retirement}</td>
+          </tr>
+          <tr>
+              <td>Miscellaneous</td>
+              <td>{this.state.miscellaneous}</td>
+          </tr>
+          <tr>
+              <td>Payment</td>
+              <td>{this.state.payment}</td>
+          </tr>
+          
+      </tbody>
+  </table>
+     )
   }
 }
 
 
-/*
-calculate(income, taxes, debt){
-var cash = income-taxes;
-
-//Necesities
-var rent = 1/3*cash;
-var foodMin = 250;
-var foodMax = .1*cash
-
-var utilities = .05*cash;
-var insuranceMax = .1*cash;
-var insuranceMin- = .03*cash;
-var transport = 
-var personal_maintence = 
 
 
-//Extra
-var savings=.1*cash;
-var retirement/investments = .1*cash;
-var entertainment
-var emergency
-var miscellaneous
 
-var[]
-}
-*/
+
+
 
 
 
